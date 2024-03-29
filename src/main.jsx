91595e1255758965ef63f3584d50951af3712ccb
YBarreraVanegas <yojansmithbarrera@gmail.com>
2024-03-28 23:08:39 -0500

    hash

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
